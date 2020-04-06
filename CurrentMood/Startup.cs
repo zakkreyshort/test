a5a8f64e363e537466a1fca5d724b09038bee213
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-04-06 14:18:24 -0700

    update project structure
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambovh@gmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- CurrentMood/Startup.cs
+++ CurrentMood/Startup.cs
@@ -26,6 +26,24 @@ namespace CurrentMood
         {
             services.AddRazorPages();
             services.AddSignalR();
+            services.AddMvc();
+            services.AddEntityFrameworkMySql()
+                .AddDbContext<CurrentMoodContext>(options => options
+                .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<CurrentMoodContext>()
+                    .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,12 +65,24 @@ namespace CurrentMood
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
                 endpoints.MapHub<ChatHub>("/chatHub");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Something went wrong!");
             });
         }
     }
